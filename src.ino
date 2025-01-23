8dc591386f02eda19dd6d8f81b2d82b9ceef0748
Andreas <1990andreaslindblad@gmail.com>
2025-01-23 12:15:36 +0100

    Lunch push

--- src.ino
+++ src.ino
@@ -1,67 +1,38 @@
-// Test Developtment
-  // For hardware testing
-
-    //Declaration
-//General
-int driverClock = 0; // Cycle counter to manage the program
-unsigned long cooldownTimer = 0; // Cooldown counter to prevent functions to execute
-
-// Declared IO
-const int motorOutput = 9; // Digital output for pump/motor
-const int ledGreen1 = 2; // Digital output for LED(Green)
-const int ledRed1 =3; // Digital output for LED(Red)
-const int ledYellow1 =4; // Digital output for LED(Yellow)
-const int  moistSensor1 = A1; // Analog input for sensor (moist)
+// Logic Dev Branch
+// Pre-Build
 
 
-//Declared varibels
-int moistStatus = 0; // Value of moisture
+const int DIGITAL_PIN = 3;
+const int ANALOG_PIN = 0;
+const int SENSOR_POWER = 2;
+int moistVal;
+bool isMoist = false;
+String moisture;
+void setup() {
 
-    //Functions Section
-
-
-  // Functions
-
-void blinkLED(int ledPin) {
-  	for (int i = 1; i <= 50; i++) {
-    digitalWrite(ledPin, HIGH); // LED ON
-    delay(100);           // wait
-    digitalWrite(ledPin, LOW);  // LED OFF
-    delay(100);           // wait
-    }
+Serial.begin(9600);
+pinMode(DIGITAL_PIN, INPUT);
+pinMode(SENSOR_POWER, OUTPUT);
+pinMode(ANALOG_PIN, INPUT);
+digitalWrite(SENSOR_POWER, LOW);
 }
-
-
-
-
-
-
-void setup()
-{
-  pinMode(motorOutput, OUTPUT);
-  pinMode(ledGreen1, OUTPUT);
-  pinMode(ledRed1, OUTPUT);
-  pinMode(moistSensor1, INPUT);
-  pinMode(A0, OUTPUT);
-  pinMode(ledYellow1,OUTPUT);
-
-  Serial.begin(9600);
-}
-void loop()
-{
-
-
-moistStatus = analogRead(moistSensor1);
-
-if (moistStatus>= 867/2){
-  digitalWrite(motorOutput, HIGH);
-  blinkLED(ledYellow1);
-  digitalWrite(motorOutput, LOW);
-}
-
-Serial.println(moistStatus);
- 
-}
-
-
-
+void loop() {
+digitalWrite(SENSOR_POWER, HIGH);
+delay(10);
+ moistVal = analogRead(ANALOG_PIN);
+ isMoist = digitalRead(DIGITAL_PIN);
+digitalWrite(SENSOR_POWER, LOW);
+if (isMoist==false) {
+ moisture = "No";
+ }
+else {
+ moisture = "Yes";
+ }
+ moistVal = map(moistVal, 0, 1023, 100, 0);
+Serial.print("Raining: ");
+Serial.println(moisture);
+Serial.print("Moisture: ");
+Serial.print(moistVal);
+Serial.println("%\n");
+delay(10000);
+} 
